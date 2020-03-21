d96e90fdde4797663303eae2ddb559edcf272009
Tyler Mikev <atmikev@gmail.com>
2020-03-20 21:26:48 -0500

    Vue bby

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,14 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::view('/{path?}', 'app');
-
-Auth::routes();
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return view('layouts/app');
+});
